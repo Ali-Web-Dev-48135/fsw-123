d6ea3e9ef9c1e77323f7874d830bd532485e9fa4
Ali Al Dairawi <ali.aldairawi@bryanuniversity.edu>
2022-05-20 10:42:25 -0700

    Adjusted The Files. Resubmitting For Re-mastery.

--- Week1/Week1-dir-one/my-app-wk1-123/src/App.js
+++ Week1/Week1-dir-one/my-app-wk1-123/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ToDoList from "./components/ToDoElement/ToDoList";
 
 
@@ -6,32 +5,10 @@ import ToDoList from "./components/ToDoElement/ToDoList";
 
 const App = () => {
 
-  const toDoArray = [
-    {
-        id: 1,
-        title: 'Take Out The Trash'   
-    },
-    {
-        id: 2,
-        title: 'Pay The Bills'
-    },
-    {
-        id: 3,
-        title: 'Wash The Car'
-    },
-    {
-        id: 4,
-        title: 'Go To Dentist'
-    },
-    {
-        id: 5,
-        title: 'Start With A React Project'
-    }
-];
   return (
     <div>
       <h1>List Of Todos.</h1>
-      <ToDoList toDoList={toDoArray}/>
+      <ToDoList/>
     </div>
   );
 }
--- Week1/Week1-dir-one/my-app-wk1-123/src/components/ToDoElement/ToDoList.jsx
+++ Week1/Week1-dir-one/my-app-wk1-123/src/components/ToDoElement/ToDoList.jsx
@@ -1,11 +1,52 @@
-import {React, Fragment} from "react";
-import ToDo from './ToDo';
+import './ToDoList.css';
 
 const ToDoList = (props) => {
+    
+  const toDoArray = [
+    {
+        id: 1,
+        title: 'Take Out The Trash'   
+    },
+    {
+        id: 2,
+        title: 'Pay The Bills'
+    },
+    {
+        id: 3,
+        title: 'Wash The Car'
+    },
+    {
+        id: 4,
+        title: 'Go To Dentist'
+    },
+    {
+        id: 5,
+        title: 'Start With A React Project'
+    }
+];
     return (
-        <Fragment>
-            <ToDo arrayList = {props.toDoList}/>
-        </Fragment>
+        <ul className='unorderedList'>
+            <li>
+                <input type="checkbox"/>
+                {toDoArray[0].title}
+            </li>
+            <li>
+                <input type="checkbox"/>
+                {toDoArray[1].title}
+            </li>
+            <li>
+                <input type="checkbox"/>
+                {toDoArray[2].title}
+            </li>
+            <li>
+                <input type="checkbox"/>
+                {toDoArray[3].title}
+            </li>
+            <li>
+                <input type="checkbox"/>
+                {toDoArray[4].title}
+            </li>
+        </ul>
 
     )
 
